e3427c34b57d0c7db1b6cbe369ef93e47565a80d
Szedlár Krisztina Mercédesz <szedlar.k.mercedesz@szelezi.dual>
2022-11-14 13:03:01 +0100

    fájl létrehozása, JFrame megcsinálása - alap

---
+++ src/oopguid/OOPGUID.java
@@ -0,0 +1,16 @@
+package oopguid;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class OOPGUID {
+    
+    //public JFrame frame;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Dolgozat");
+        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        
+    }
+    
+}
